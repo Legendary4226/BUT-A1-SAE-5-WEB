158e854cc0c0e54a7abc3b0dbbf924b2d07f6a95
RichardGuilland <guillandrichard@gmail.com>
2021-11-07 11:04:34 +0100

    Mise en place des bases (feuilles de styles, pages html)

--- index.php
+++ index.php
@@ -1,15 +1,36 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>BUT Informatique Lyon1</title>
+    <link rel="stylesheet" href="css/colors.css">
 </head>
 <body>
+    <main>
+        <!-- Importation du menu de gauche pour naviguer sur le contenu -->
+        <?php
+        
+        ?>
 
-    <!-- Importation du FOOTER -->
-    <?php
-    include("footer.html");
-    ?>
+
+        <!-- Importation du contenu -->
+        <?php
+        if (isset($GET["page"])) {
+
+        } else {
+            include("contenu1.html");
+        }
+        ?>
+
+
+        <!-- Menu fixe pour naviguer entre pages -->
+        
+
+
+        <!-- Importation du FOOTER -->
+        <?php
+        include("footer.html");
+        ?>
+    </main>
 </body>
 </html>
