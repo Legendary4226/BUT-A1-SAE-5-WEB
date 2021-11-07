b6e8a395f473b3dba537782e153f7ebf07e32e91
Angelo Naudts <naudtsangelo@gmail.com>
2021-11-07 12:08:10 +0100

    Ajout et modification de footer.html, ajout de footer.css

--- index.php
+++ index.php
@@ -1,36 +1,22 @@
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BUT Informatique Lyon1</title>
     <link rel="stylesheet" href="css/colors.css">
 </head>
+
 <body>
     <main>
-        <!-- Importation du menu de gauche pour naviguer sur le contenu -->
-        <?php
-        
-        ?>
 
-
-        <!-- Importation du contenu -->
-        <?php
-        if (isset($GET["page"])) {
-
-        } else {
-            include("contenu1.html");
-        }
-        ?>
-
-
-        <!-- Menu fixe pour naviguer entre pages -->
-        
 
 
         <!-- Importation du FOOTER -->
         <?php
-        include("footer.html");
+        include("pages/footer.html");
         ?>
     </main>
 </body>
+
 </html>
