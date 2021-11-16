05e7eda60f9edff56da1e1f573da85773442cd51
Guilland <richard.guilland@etu.univ-lyon1.fr>
2021-11-16 08:18:30 +0100

    Modifications majeure du menu, des sous menu. Test de 3D pour le bouton d apparition du menu.

--- index.php
+++ index.php
@@ -8,48 +8,111 @@
     <link rel="stylesheet" href="sass/index.css">
     <link rel="stylesheet" href="sass/error404.css">
     <link rel="stylesheet" href="css/footer.css">
+
+    <script src="js/top-menu.js"></script>
 </head>
 
 <body>
+
+    <!-- Menu fixe pour naviguer entre pages -->
+    <nav class="top-menu">
+            <div id="slide-menu">
+                <ul>
+                    <li>
+                        <a href="?page=index">But en général</a>    
+                    </li>
+                    <li>
+                        <a href="?page=arround">Autour de l'IUT</a>
+                    </li>
+                    <li>
+                        <a href="?page=but">Le BUT Informatique</a>
+                    </li>
+                    <li>
+                        <a href="?page=404">Formulaire</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="button-menu">
+                <div onClick="toggle_top_menu()" id="cube-3D" class="menu-cube">
+                    <div class="front"><p>Menu</p></div>
+                    <div class="back"></div>
+                    <div class="left"></div>
+                    <div class="right"></div>
+                    <div class="top"></div>
+                    <div class="bottom"></div>
+                </div>
+            </div>
+        </nav>
+
     <main>
         <!-- Menu de gauche pour naviguer sur le contenu -->
-        
+        <nav class="sub-left-menu">
+            <?php
+            if (isset($_GET['page'])) {
+                switch ($_GET['page']) {
+                    case 'index':
+                        include("pages-sub-menus/sub1.html");
+                        break;
+                    case 'arround':
+                        include("pages-sub-menus/sub2.html");
+                        break;
+                    case 'but':
+                        include("pages-sub-menus/sub3.html");
+                        break;
+                    case 'question-form':
+                        //include("pages-sub-menus/");
+                        break;
+                }
+            } else {
+                include("pages-sub-menus/sub1.html");
+            }
+            ?>
+        </nav>
 
 
         <!-- Importation du contenu -->
-        <?php
-        if (isset($_GET['page'])) {
-            switch ($_GET['page']) {
-                case 'index':
-                    include("pages/contenu1.html");
-                    break;
-                case 'arround':
-                    include("pages/contenu2.html");
-                    break;
-                case 'but':
-                    include("pages/contenu3.html");
-                    break;
-                case 'question-form':
-                    include("pages/formulaire.html");
-                    break;
-                default:
-                    include("pages/error404.html");
+        <section class="content">
+            <?php
+            if (isset($_GET['page'])) {
+                switch ($_GET['page']) {
+                    case 'index':
+                        include("pages/contenu1.html");
+                        break;
+                    case 'arround':
+                        include("pages/contenu2.html");
+                        break;
+                    case 'but':
+                        include("pages/contenu3.html");
+                        break;
+                    case 'question-form':
+                        include("pages/formulaire.html");
+                        break;
+                    default:
+                        include("pages/error404.html");
+                }
+            } else {
+                include("pages/contenu1.html");
             }
-        } else {
-            include("pages/contenu1.html");
-        }
-        ?>
-
-
-        <!-- Menu fixe pour naviguer entre pages -->
+            ?>
+        </section>
         
-
-
-        <!-- Importation du FOOTER -->
-        <?php
-        include("pages/footer.html");
-        ?>
     </main>
+
+    <!-- Importation du FOOTER -->
+    <?php
+    if (isset($_GET['page'])) {
+        switch ($_GET['page']) {
+            case 'index':
+            case 'arround':
+            case 'but':
+            case 'question-form':
+                include("pages/footer.html");
+                break;
+        }
+    } else {
+        include("pages/footer.html");
+    }
+    ?>
 </body>
 
 </html>
---
+++ js/top-menu.js
@@ -0,0 +1,4 @@
+function toggle_top_menu() {
+    document.getElementById("slide-menu").classList.toggle("slide-menu-on");
+    document.getElementById("cube-3D").classList.toggle("menu-on");
+};
